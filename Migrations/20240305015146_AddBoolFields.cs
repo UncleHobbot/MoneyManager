169be6318b46f96361259e4260443bcd3312d948
Max Iliashenko <uncle.ho.max@gmail.com>
2024-03-04 22:35:48 -0500

    Import Mint transactions

---
+++ Migrations/20240305015146_AddBoolFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBoolFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRuleApplied",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNew",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHideFromGraph",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRuleApplied",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsNew",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsHideFromGraph",
+                table: "Accounts");
+        }
+    }
+}
---
+++ Model/Import/ImportTypeEnum.cs
@@ -0,0 +1,6 @@
+﻿namespace MoneyManager.Model.Import;
+
+public class ImportTypeEnum
+{
+    
+}
