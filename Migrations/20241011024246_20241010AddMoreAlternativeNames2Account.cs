c4c0ea02d48ffc0eb227e33a92f248e08c829416
Max Iliashenko <uncle.ho.max@gmail.com>
2024-11-02 20:54:26 -0400

    Migration

---
+++ Migrations/20241011024246_20241010AddMoreAlternativeNames2Account.cs
@@ -0,0 +1,297 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20241010AddMoreAlternativeNames2Account : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Balances_Accounts_AccountId",
+                table: "Balances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rules_Categories_CategoryId",
+                table: "Rules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalDescription",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalDescription",
+                table: "Rules",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NewDescription",
+                table: "Rules",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Rules",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "Balances",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShownName",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlternativeName3",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlternativeName4",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlternativeName5",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Balances_Accounts_AccountId",
+                table: "Balances",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rules_Categories_CategoryId",
+                table: "Rules",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Balances_Accounts_AccountId",
+                table: "Balances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rules_Categories_CategoryId",
+                table: "Rules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "AlternativeName3",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "AlternativeName4",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "AlternativeName5",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalDescription",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalDescription",
+                table: "Rules",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NewDescription",
+                table: "Rules",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Rules",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "Balances",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShownName",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Balances_Accounts_AccountId",
+                table: "Balances",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rules_Categories_CategoryId",
+                table: "Rules",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+    }
+}
